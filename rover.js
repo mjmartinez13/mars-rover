b89d5b6cb52e89fde12481eea1403454b60494c9
Marlon Martinez <jmarlonmartinez13@gmail.com>
2017-01-27 13:44:09 -0500

    update-2

--- rover.js
+++ rover.js
@@ -1,25 +1,173 @@
-var myRover = {
-  position: [0,0], 
+
+//===============================
+//  ARROW KEY JS
+//===============================
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    switch (evt.keyCode) {
+        case 38:
+          goForward(rover);
+            break;
+        case 37:
+            goLeft(rover);
+            break;
+        case 39:
+            goRight(rover);
+            break;
+        case 40:
+            goBackward(rover);
+    }
+};
+
+//===============================
+//  MY ROVER
+//===============================
+
+
+var rover = {
+  position: [0,0],
   direction: 'N'
 };
+
+//===============================
+//  ROCK
+//===============================
+
+var rock = {
+  position: [5,0],
+};
+
+var rock2 = {
+  position: [4,5],
+};
+
+
+//===============================
+//  GO FORWARD
+//===============================
 
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
+      rover.position[0]++ ;
       break;
     case 'E':
-      rover.position[1]++
+      rover.position[1]++ ;
       break;
     case 'S':
-      rover.position[0]--
+      rover.position[0]-- ;
       break;
     case 'W':
-      rover.position[1]--
+      rover.position[1]-- ;
       break;
-  };
+    }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+
+  roverGrid(rover);
+  myRock(rover);
+  document.getElementById('rover-position').innerHTML = "[" + rover.position[0] + ", " + rover.position[1] + "]";
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
 }
 
-goForward(myRover);
+//===============================
+//  GO BACKWARD
+//===============================
+
+function goBackward(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]-- ;
+      break;
+    case 'E':
+      rover.position[1]-- ;
+      break;
+    case 'S':
+      rover.position[0]++ ;
+      break;
+    case 'W':
+      rover.position[1]++ ;
+      break;
+  }
+  roverGrid(rover);
+  document.getElementById('rover-position').innerHTML = "[" + rover.position[0] + ", " + rover.position[1] + "]";
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+}
+
+//===============================
+//  GO RIGHT
+//===============================
+
+function goRight(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E';
+      break;
+    case 'E':
+      rover.direction = 'S' ;
+      break;
+    case 'S':
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'N';
+      break;
+
+    }
+  roverGrid(rover);
+  document.getElementById('rover-position').innerHTML = "[" + rover.direction + "]";
+  console.log("New Rover Direction: [" + rover.direction + "]");
+}
+
+//===============================
+//  GO LEFET
+//===============================
+
+function goLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W';
+      break;
+    case 'E':
+      rover.direction = 'N' ;
+      break;
+    case 'S':
+      rover.direction = 'E';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+
+    }
+ roverGrid(rover);
+  document.getElementById('rover-position').innerHTML = "[" + rover.direction + "]";
+  console.log("New Rover Direction: [" + rover.direction + "]");
+}
+//===============================
+//  ROVER GRID
+//===============================
+
+  function roverGrid(rover) {
+    if (rover.position[0] >= 9) {
+      rover.position[0] = 0;
+    }else if (rover.position[1] > 9){
+      rover.position[1] = 0;
+    }else if (rover.position[0] < 0) {
+      rover.position[0] = 9;
+    }else if (rover.position[1] < 0) {
+      rover.position[1] = 9;
+    }
+  }
+
+  //===============================
+  //  ROCK
+  //===============================
+function myRock(rover){
+  if (rover.position >= rock.position ) {
+    rover.position[0] = 4;
+    alert("\u2666\u2666WARNING\u2666\u2666 rock ahead, change direction \u25c0\u25b6");
+  }else if (rover.position >= rock2.position) {
+    rover.position[1] = 4;
+    alert("\u2666\u2666WARNING\u2666\u2666 rock ahead, change direction \u25c0\u25b6");
+  }
+}
